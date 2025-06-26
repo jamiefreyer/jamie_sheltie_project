e908a941bb9f0025d84ca68a9f9b68d77e96c83e
jamiefreyer <87341262+jamiefreyer@users.noreply.github.com>
2025-06-26 00:09:12 +0000

    holy shit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "A Biographical Homepage: This should provide an overview of your academic background, career aspirations, professional interests, and a brief personal introduction. You could include elements such as your interests, hobbies, or a unique aspect about yourself to give it a personal touch. It's a place to showcase your personality alongside your academic and professional achievements.)."
 )
